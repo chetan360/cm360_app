940b0bb541b5dc95388137976f1c17c2be9453ba
chetan360 <chetansmankar360@gmail.com>
2024-11-02 13:07:28 +0530

    image issue resolve

--- src/App.jsx
+++ src/App.jsx
@@ -8,37 +8,37 @@ function App() {
       name: "Johnsan Wood",
       role: "Front End Developer",
       email: "Johnsanwood@microft.com",
-      image: "../public/1.jpg",
+      image: "/1.jpg",
     },
     {
       name: "John Doe",
       role: "Team Lead",
       email: "Johndoe@microft.com",
-      image: "../public/2.jpg",
+      image: "/2.jpg",
     },
     {
       name: "Fakhar Naveed",
       role: "UI/UX Designer",
       email: "Fakharnaveed@microft.com",
-      image: "../public/3.jpg",
+      image: "/3.jpg",
     },
     {
       name: "Alex Made",
       role: "Software Engneer",
       email: "Alexmade@microft.com",
-      image: "../public/4.jpg",
+      image: "/4.jpg",
     },
     {
       name: "Jane Lane",
       role: "Software Engneer",
       email: "Janelane@microft.com",
-      image: "../public/5.jpg",
+      image: "/5.jpg",
     },
     {
       name: "Amma Stark",
       role: "Software Engneer",
       email: "Ammastark@microft.com",
-      image: "../public/6.jpg",
+      image: "/6.jpg",
     },
   ]);
 
